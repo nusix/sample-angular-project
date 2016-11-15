7a5de338206618d605186f6f776ea4bb014776af
Nusix <zdenokrnac@gmail.com>
2016-11-15 16:45:42 +0100

    Added karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -37,13 +37,22 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'index/index.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    // reporters: ['progress'],
+
+    reporters: ['progress','coverage'],
+
+    //default options for coverage
+    coverageReporter: {
+        type : 'html',
+        dir : 'coverage/'
+    },
 
 
     // web server port
